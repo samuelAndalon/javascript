3b7f776cde6282cb2f3e3de9f11b41d05923e046
Samuel <sam_2f@hotmail.com>
2018-11-11 20:15:36 -0800

    m

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,5 +1,49 @@
-describe('test', () => {
-    it('should work', () => {
+const {rotateArray} = require('../index.js');
+const {getPeriod} = require('../index.js');
 
+describe('rotateArray', () => {
+  it('should work', () => {
+    expect(
+      rotateArray([1, 2, 3, 4, 5], 10000000000000002)
+    ).toEqual(
+      [4, 5, 1, 2, 3]
+    );
+  });
+});
+
+describe('getPeriod', () => {
+  it('should find n = 3 and P = "ab" for "ababab"', () => {
+    expect(
+      getPeriod('ababab')
+    ).toEqual({
+      'n': 3,
+      'P': 'ab'
     });
+  });
+
+  it('should find n = 1 and P = "x" for "xxxxxx"', () => {
+    expect(
+      getPeriod('xxxxxx')
+    ).toEqual({
+      'n': 6,
+      'P': 'x'
+    });
+  });
+
+  it('should find n = 2 and P = "aabba" for "aabbaaabba"', () => {
+    expect(
+      getPeriod('aabbaaabba')
+    ).toEqual({
+      'n': 2,
+      'P': 'aabba'
+    });
+  });
+  it('should not find n and P for "aabbaaabbab"', () => {
+    expect(
+      getPeriod('aabbaaabbab')
+    ).toEqual({
+      'n': 0,
+      'P': ''
+    });
+  });
 });
---
+++ src/index.js
@@ -0,0 +1,54 @@
+/**
+ * Rotate array values to the right
+ * @param  {array} array
+ * @param  {number} positions
+ * @returns rotatedArray
+ */
+exports.rotateArray = (array, positions) => {
+  var rotatedArray = [];
+  var _positions = positions % array.length;
+  var newPosition;
+  for (var i = 0; i < array.length; i++) {
+    newPosition = i + _positions;
+    rotatedArray[
+      newPosition < array.length
+        ? newPosition
+        : newPosition - array.length
+    ] = array[i];
+  }
+  return rotatedArray;
+}
+
+/**
+ * Find whether string S is periodic. 
+ * Periodic indicates S = nP. 
+ * e.g. 
+ * S = "ababab", then n = 3, and P = "ab" 
+ * S = "xxxxxx", then n = 6, and P = "x" 
+ * S = "aabbaaabba", then n = 2, and P = "aabba" 
+ * @param  {string} string
+ * @returns {Object} object that contains n and p
+ */
+exports.getPeriod = (string) => {
+  let half = Math.ceil(string.length / 2);
+  for (let i = 1; i <= half; i++) {
+    if ((string.length % i) === 0) {
+      let period = string.slice(0, i);
+      let periodTimesInString = string.length / period.length;
+      let matchedPeriodInSubstrings = 0;
+      for(let i = 0; i < periodTimesInString && string.substr(i * period.length, period.length) === period; i++) {
+        matchedPeriodInSubstrings++;
+      }
+      if(matchedPeriodInSubstrings === periodTimesInString) {
+        return {
+          'n': matchedPeriodInSubstrings,
+          'P': period
+        }
+      }
+    }
+  }
+  return {
+    'n': 0,
+    'P': ''
+  };
+}
